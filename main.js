58ca37a4033eea1297f531f704ca47b96ebb05f4
“[firstname <rabya759@gmail.com>
2024-05-03 23:55:58 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// import inquirer from "inquirer"
+// let ans = await inquirer.prompt({
+// name : "age",
+// type : "number",
+// message : "Enter Your Age:"
+// });
+// console.log ("Insha ALLAH , in" + (60 - ans.age)+ "Years Old")
+import inquirer from "inquirer";
+let ans = await inquirer.prompt({
+    // name : "fristNumber",
+    name: "firstNum",
+    type: "number",
+    message: "enter your num"
+});
+let ans1 = await inquirer.prompt({
+    name: "secondNum",
+    type: "number",
+    message: "enter your snum"
+});
+console.log("your calcution is," + (ans.firstNum + ans1.secondNum));
